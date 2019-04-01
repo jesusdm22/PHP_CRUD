2887e70c2df891af54338e6806a793dbebe27f95
Jesús Díaz Muñoz <jesusdm22@hotmail.com>
2019-04-01 20:36:32 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Timer</title>
+	<!--Estilos-->
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/my_style.css">
+
+	<!--Scripts-->
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+	<script src="bootstrap/js/jquery-3.2.1.min.js"></script>
+</head>
+<body>
+
+</body>
+</html>
