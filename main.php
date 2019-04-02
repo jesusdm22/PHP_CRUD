644753381c8c74b123b4b102594550b407e9ccce
Jesús Díaz Muñoz <jesusdm22@hotmail.com>
2019-04-02 10:00:57 +0200

    Actualizacion main.php

--- main.php
+++ main.php
@@ -4,10 +4,21 @@
 //----------------------------------
 
 function testConn() {
-	if(connect())
-	echo "Conexión correcta!";
-	else
-	echo "No hay conexión";
+	$conn=connect();
+	// Check connection
+	if (mysqli_connect_errno())
+	  {
+	  echo "Error al conectar a la base de datos. " . mysqli_connect_error();
+	  }
+
+	// Perform a query, check for error
+	if (!mysqli_query($conn,"INSERT INTO debug (message) VALUES ('ProbandoDB')"))
+	  {
+	  echo("Descripción del error:  " . mysqli_error($conn));
+	  }
+
+	mysqli_close($conn);
+
 }
 
 
@@ -15,25 +26,24 @@ function testConn() {
 //FUNCIONES ---------------------
 //-------------------------------
 
+//Datos de la BD
+$dbhost = "localhost";
+$dbuser = "prueba";
+$dbpwd = "prueba";
+$dbname = "crudapp";
+$conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+
 //Funcion para conectar a la BD
 function connect() {
-	//Para la DB
-	$dbhost = "localhost";
-	$dbuser = "root";
-	$dbpwd = "";
-	$dbname = "cms";
-	$conn;
-
-	global $dbhost, $dbuser, $dbpwd, $dbname, $conn;
-	$conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+	return  $GLOBALS['conn'];
 }
 
 //Funcion para recuperar los datos del formulario
 function  CatchFromForm() {
 	$datosEmpleado = array(
-		'nombre' => $_POST['nombre'],
-		'sede' => $_POST['sede'],
-		'dpto' => $_POST['dpto']
+		0 => $_POST['nombre'],
+		1 => $_POST['sede'],
+		2 => $_POST['dpto']
 	);
 
 	return $datosEmpleado;
@@ -47,14 +57,26 @@ function GetFromDB() {
 	}
 }
 
+function SetEmpleado()
+
 //----------------------------------------------------------------------
 //EJECUCIÓN DEL SCRIPT -------------------------------------------------
 //----------------------------------------------------------------------
 
-//Probamos la conexión
-testConn();
-
+//1º Paso
 //Guardamos los datos del formulario en un array
-$losdatos = CatchFromForm();
+$datos = CatchFromForm();
+
+//Llamamos a la conexión
+$conn = connect();
+
+//Creamos la query
+$sql = "INSERT INTO empleados (nombre, sede, departamento)
+        VALUES ('$datos[0]', '$datos[1]', '$datos[2]')";
+
+if (!mysqli_query($conn,$sql))
+	{
+	echo("Descripción del error:  " . mysqli_error($conn));
+	}
 
 ?>
